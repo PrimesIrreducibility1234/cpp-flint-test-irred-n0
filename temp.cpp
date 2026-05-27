dde0e2ffb60f90e683515e7fd7df565a50984377
PrimesIrreducibility1234 <mathlatex123@gmail.com>
2026-05-26 19:53:34 -0700

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -257,7 +257,7 @@ bool dfsFactorization(int idx, const fmpz_poly_t F, const fmpz_poly_factor_t fac
 
 		if (validPoly(g)) {
     		if (fmpz_poly_divides(h, F, g)) {
-        		if (validPoly(h)) {
+        		if (validPoly(h))
 				ok = true;
 				/*cout << "Factorization in N0: (";
 				fmpz_poly_print_pretty(g, "x");
